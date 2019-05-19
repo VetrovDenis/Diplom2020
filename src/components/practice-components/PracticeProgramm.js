5016d9f02b8547b0c0f185c10daec754cbe37585
Denis Vetrov <denisvetrov@MacBook-Air-Denis.local>
2019-05-19 13:18:19 +0300

    Add some calculations at practice-service & add conditional rendered form

--- src/components/practice-components/PracticeProgramm.js
+++ src/components/practice-components/PracticeProgramm.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './PracticeProgramm.css';
 
-
 export default class PracticeProgramm extends React.Component {
     state = {
+        g: 10
+    }
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
     }
     render() {
+        const inputValues = [
+            {
+                label: "G:",
+                name: "g",
+                type: "number",
+                value: this.state.g
+            }
+        ]
         return (
             <div className="Main">
-                <div className="Back-button" onClick={()=>this.props.navigateTo()}>
+                <div className="Back-button" onClick={() => this.props.navigateTo()}>
                     Back
                 </div>
-                Programm realisation is in progress
+                <form>
+                    {inputValues.map((inputInfo, index) => {
+                        return (
+                            <label key={index}>
+                                {inputInfo.label}
+                                <input type={inputInfo.type} name={inputInfo.name} value={inputInfo.value} onChange={this.handleChange} />
+                            </label>
+                        )
+                    })}
+                </form>
             </div>
         );
     }
