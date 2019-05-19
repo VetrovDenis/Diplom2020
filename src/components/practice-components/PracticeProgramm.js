ae174d5bdc54db2a22aaecd19b0b8b825e882ea1
Denis Vetrov <denisvetrov@MacBook-Air-Denis.local>
2019-05-19 15:46:16 +0300

    Add chart diagram

--- src/components/practice-components/PracticeProgramm.js
+++ src/components/practice-components/PracticeProgramm.js
@@ -1,20 +1,162 @@
 import React from 'react';
 import './PracticeProgramm.css';
+import Chart from "chart.js"
+import { calculateTwoPhaseSkin } from "../../services/sheet/two-phase-skin"
 
 export default class PracticeProgramm extends React.Component {
     state = {
-        g: 10
+        g: 10,
+        δ1: 1,
+        δ2: 1,
+        p1: 1,
+        p2: 2,
+        υ1: 1,
+        υ2: 2,
+        μ1: 1,
+        μ2: 2,
+        Ge: 0,
+        maxSpeed: null,
+        averageSpeed: null,
+        geOpt: null,
+        yLength: 10
+    }
+    componentDidMount = () => {
+        this.calculateChart()
     }
     handleChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value })
+        if (event.target.value.length > 0) {
+            this.setState({ [event.target.name]: parseFloat(event.target.value) })
+        }
     }
+    clearInput = (event) => {
+        //this.setState({ [event.target.name]: 0 })
+    }
+    calculateChart = () => {
+        const { g, δ1, δ2, p1, p2, υ1, υ2, μ1, μ2, Ge, yLength } = this.state
+        const calculateInfo = calculateTwoPhaseSkin(g, δ1, δ2, p1, p2, υ1, υ2, μ1, μ2, Ge, yLength)
+        console.log(calculateInfo.speedArray)
+        this.setState({
+            maxSpeed: calculateInfo.Wmax,
+            averageSpeed: calculateInfo.Wsr,
+            geOpt: calculateInfo.GeOpt
+        })
+        let w1Array = [], w2Array = [], indexArray = []
+        calculateInfo.speedArray.forEach(speedElement => {
+            w1Array.push(speedElement.W1)
+            w2Array.push(speedElement.W2)
+            indexArray.push(speedElement.y.toFixed(1))
+        });
+        var myLineChart = new Chart(this.refs.myChart, {
+            type: 'line',
+            data: {
+                labels: indexArray,
+                datasets: [{
+                    label: 'Профіль швидкості першої плівки',
+                    data: w1Array,
+                    borderColor: 'rgba(255, 99, 132, 0.8)',
+                    fill: false,
+                    borderWidth: 1
+                },
+                {
+
+                    label: 'Профіль швидкості другої плівки',
+                    data: w2Array,
+                    borderColor: 'rgba(255, 206, 86, 0.8)',
+                    fill: false,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+
     render() {
         const inputValues = [
             {
-                label: "G:",
+
+                label: "G (прискорення вільного падіння):",
                 name: "g",
                 type: "number",
                 value: this.state.g
+            },
+            {
+
+                label: "δ1 (товщина рідини першої плівки):",
+                name: "δ1",
+                type: "number",
+                value: this.state.δ1
+            },
+            {
+
+                label: "δ2 (товщина рідини першої плівки):",
+                name: "δ2",
+                type: "number",
+                value: this.state.δ2
+            },
+            {
+
+                label: "p1 (густина рідини першої плівки):",
+                name: "p1",
+                type: "number",
+                value: this.state.p1
+            },
+            {
+
+                label: "p2 (густина рідини другої плівки):",
+                name: "p2",
+                type: "number",
+                value: this.state.p2
+            },
+            {
+
+                label: "υ1 (кінематичний коефіцієнти в’язкості рідини першої плівки):",
+                name: "υ1",
+                type: "number",
+                value: this.state.υ1
+            },
+            {
+
+                label: "υ2 (кінематичний коефіцієнти в’язкості рідини першої плівки):",
+                name: "υ2",
+                type: "number",
+                value: this.state.υ2
+            },
+            {
+
+                label: "μ1 (динамічний коефіцієнти в’язкості рідини першої плівки):",
+                name: "μ1",
+                type: "number",
+                value: this.state.μ1
+            },
+            {
+
+                label: "μ2 (динамічний коефіцієнти в’язкості рідини першої плівки):",
+                name: "μ2",
+                type: "number",
+                value: this.state.μ2
+            },
+            {
+
+                label: "Ge (безрозмірний комплекс, що характеризує вплив газового потоку на плівку):",
+                name: "Ge",
+                type: "number",
+                value: this.state.Ge
+            }
+            ,
+            {
+
+                label: "y (максимальная відстань від пластини):",
+                name: "yLength",
+                type: "number",
+                value: this.state.yLength
             }
         ]
         return (
@@ -22,16 +164,32 @@ export default class PracticeProgramm extends React.Component {
                 <div className="Back-button" onClick={() => this.props.navigateTo()}>
                     Back
                 </div>
-                <form>
-                    {inputValues.map((inputInfo, index) => {
-                        return (
-                            <label key={index}>
-                                {inputInfo.label}
-                                <input type={inputInfo.type} name={inputInfo.name} value={inputInfo.value} onChange={this.handleChange} />
-                            </label>
-                        )
-                    })}
-                </form>
+                <div className="Form-and-chart-container">
+                    <div className="Input-form-container">
+                        <form className="Input-form">
+                            {
+                                inputValues.map((inputInfo, index) => {
+                                    return (
+                                        <label className="Input-label" key={index}>
+                                            {inputInfo.label}
+                                            <input className="Input" type={inputInfo.type} name={inputInfo.name} value={inputInfo.value} onChange={this.handleChange} onFocus={this.clearInput} />
+                                        </label>
+                                    )
+                                })}
+                            <div className="Submit-button" onClick={() => {
+                                this.calculateChart()
+                            }}>
+                                Submit
+                        </div>
+                        </form>
+                    </div>
+                    <div className="Chart-container">
+                        <canvas ref="myChart" ></canvas>
+                    </div>
+                </div>
+                {this.state.maxSpeed && <div>Максимальне значенн швидкості: {this.state.maxSpeed}</div>}
+                {this.state.averageSpeed && <div>Среденє значення швидкості: {this.state.averageSpeed}</div>}
+                {this.state.geOpt && <div>Ge оптимальне: {this.state.geOpt}</div>}
             </div>
         );
     }
--- src/services/sheet/two-phase-skin.js
+++ src/services/sheet/two-phase-skin.js
@@ -1,22 +1,31 @@
-export function calculateTwoPhaseSkin(g, δ1, δ2, p1, p2, υ1, υ2, μ1, μ2, Ge) {
+export function calculateTwoPhaseSkin(g, δ1, δ2, p1, p2, υ1, υ2, μ1, μ2, Ge, yLength) {
     //g – прискорення вільного падіння
     //p – густина рідини; 
     //υ i μ - кінематичний та динамічний коефіцієнти в’язкості;
     //Ge поток газа
     //δ ширина пленки
-    let W1 = (g / υ1) * (-(y * y / 2) + (δ1 + (p2 * δ1 / p1) * (1 - Ge)) * y)
-    let W2 = (g / υ2) * (-(y * y / 2) + (δ1 + δ2 * (1 - Ge) * y + (δ1 * δ1 / 2) * ((υ2 / υ1) - 1) + δ1 * δ2 * ((μ2 / μ1) - 1) * (1 - Ge)))
+    //y-напруга тертя
+    let speedArray = []
+    let W1, W2;
+    for (let y = 1; y < Number(yLength) + 1; y = y + 0.2) {
+        W1 = ((g / υ1) * (-(y * y / 2) + (δ1 + (p2 * δ1 / p1) * (1 - Ge)) * y)).toFixed(2)
+        W2 = ((g / υ2) * (-(y * y / 2) + (δ1 + δ2 * (1 - Ge) * y + (δ1 * δ1 / 2) * ((υ2 / υ1) - 1) + δ1 * δ2 * ((μ2 / μ1) - 1) * (1 - Ge)))).toFixed(2)
+        if (W1 < 0 || W2 < 0)
+            break
+        speedArray.push({ y, W1: parseFloat(W1), W2: parseFloat(W2) })
+    }
     //оптімальна дія на робочу плівку
     let q = (δ1 * μ2) / (δ2 * μ1)
     let r = (1 / 2) * (υ1 / υ2) * (δ1 / δ2) * (δ1 / δ2)
     let GeOpt = (2 + 6 * (q + r) + Math.sqrt(1 + 12 * q * q + 6 * (1 + 6 * r) * (q + r))) / (3 + 6 * q)
+    //максимальне та середнє значення швидкості
     let υ = υ2 / υ1
     let δ = δ2 / δ1
     let p = p2 / p1
-    let Wsr = 1 / 3 - Ge / 2 + (υ * δ * δ) / 2 + δ * υ * p(1 - Ge);
-    let Wmax = (1 / 2) * (1 - Ge) * (1 - Ge) + (υ * δ * δ) / 2 + δ * υ * p(1 - Ge);
+    let Wsr = 1 / 3 - Ge / 2 + (υ * δ * δ) / 2 + δ * υ * p * (1 - Ge);
+    let Wmax = (1 / 2) * (1 - Ge) * (1 - Ge) + (υ * δ * δ) / 2 + δ * υ * p * (1 - Ge);
     let calculatedInfo = {
-        W1, W2, GeOpt, Wsr, Wmax
+        speedArray, GeOpt, Wsr, Wmax
     }
     return calculatedInfo
 }
